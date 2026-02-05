f0b0f6512201855f90b475845a48c79ff4444f1f
jShiz <70830976+jshiz@users.noreply.github.com>
2026-02-05 13:47:44 -0500

    Fix: Cleaned up favicons to satisfy all checker requirements (fixed duplication and added touch title)

--- app/layout.tsx
+++ app/layout.tsx
@@ -71,10 +71,19 @@ export const metadata: Metadata = {
       'max-snippet': -1,
     },
   },
+  appleWebApp: {
+    title: 'Frenchies R Us NY',
+    statusBarStyle: 'default',
+    capable: true,
+  },
   icons: {
-    icon: '/favicon.ico',
-    shortcut: '/favicon-96x96.png',
-    apple: '/apple-touch-icon.png',
+    icon: [
+      { url: '/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
+      { url: '/favicon.ico', sizes: 'any' },
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+    ],
     other: [
       {
         rel: 'icon',
